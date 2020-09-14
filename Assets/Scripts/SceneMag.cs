86e0763cc5ddc7d8aca94dce5ea7de714e0e2678
Fikran Akbar <fikranakbar756@gmail.com>
2020-09-14 22:40:14 +0700

    First Commit

---
+++ Assets/Scripts/SceneMag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMag : MonoBehaviour
+{
+    private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameController.IsGameEnded &&
+            SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(LoadAfterSeconds());
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+
+    private IEnumerator LoadAfterSeconds()
+    {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
